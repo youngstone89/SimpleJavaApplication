bc91f9a79a8733f990e6c293310e12d4e64107ee
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-09-12 20:31:31 +0900

    My First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import common.Util;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Change Test 1");
+        Util.print("Passing Message from Main Class");
+    }
+}
---
+++ src/common/Util.java
@@ -0,0 +1,9 @@
+package common;
+
+public class Util {
+
+    public static void print(String message){
+        System.out.println("Printing Message from Util Class::" + message);
+    }
+
+}
